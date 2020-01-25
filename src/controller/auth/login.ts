9bbee01e940e209a97a66fe196751375df35b5e4
Shin <ansrl0107@gmail.com>
2020-01-25 14:29:30 +0900

    login api 추가

---
+++ src/controller/auth/login.ts
@@ -0,0 +1,22 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { User } from '../../entity';
+import { error, success } from '../../util';
+
+interface Body {
+  email: string;
+  password: string;
+}
+
+export const login: APIGatewayProxyHandler = async (event) => {
+  const { body } = event;
+  const { email, password } = body as unknown as Body;
+  const user = await User.findOne({ where: { email } });
+  if (!user) {
+    return error('NOT_FOUND', 'USER_NOT_FOUND');
+  }
+  if (user.comparePassword(password)) {
+    return error('BAD_REQUEST', 'INVALID_PASSWORD');
+  }
+  const accessToken = await user.createAccessToken();
+  return success({ accessToken, user });
+};
